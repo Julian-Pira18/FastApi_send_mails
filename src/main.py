e63279fdee3f2eb8084ff175baf92e40776f47ea
Julian Pira <jpira@unal.edu.co>
2024-09-09 02:13:39 -0500

    Second Deploy

--- src/main.py
+++ src/main.py
@@ -14,4 +14,4 @@ app.include_router(user_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=int(os.getenv("PORT")))
+    uvicorn.run(app,  host="0.0.0.0", port=int(os.getenv("PORT")))
